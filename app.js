811a88a841291aa2f869e7e4ea93a594609c3d67
achdaisy <oduordaizy@gmail.com>
2024-09-04 08:53:27 +0300

    updated the js file

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ result = document.getElementById("result"),
 clear = document.getElementById("clear"),
 resultDisplayed = false;
 
+//Adding click handlers to number buttons
+for (var i = 0; i < number.length; i++){
+    number [i].addEventListener("click", function(e){
+
+        //storing the current input, and the last character in variables
+        var currentString = input.innerHTML;
+        
+
+
+    })
+}
 
