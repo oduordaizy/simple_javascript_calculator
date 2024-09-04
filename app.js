ff2a18c2feef6db76ef6827c3e516ce623c0e442
achdaisy <oduordaizy@gmail.com>
2024-09-04 08:39:30 +0300

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var input = document.getElementById("input"),
+number = document.querySelectorAll("numbers div"),
+operator = document.querySelectorAll("operators div"),
+result = document.getElementById("result"),
+clear = document.getElementById("clear"),
+resultDisplayed = false;
+
+
